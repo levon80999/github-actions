b60886b36cbf510ffa53d5870669deaf28e2289e
levon80999 <levonb@ucraft.com>
2022-10-19 20:25:50 +0400

    sd

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -5,8 +5,9 @@ use MongoDB\Driver\ReadPreference;
 class ConnectionTest extends \PHPUnit\Framework\TestCase {
     public function testCanBeCreatedFromValidEmailAddress(): void
     {
+        $host = getenv('MONGO_HOST');
         $client = new MongoDB\Client(
-            'mongodb://'.getenv('MONGO_HOST').'@27017/test?retryWrites=true&w=majority'
+            'mongodb://'.$host.'@27017/test?retryWrites=true&w=majority'
         );
         $primary = $client->getManager()->selectServer(new ReadPreference(ReadPreference::RP_PRIMARY));
         $this->assertNotNull($primary);
